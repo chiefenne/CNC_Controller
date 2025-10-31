f59fffc1584e20c5828e1e8fd6219f8b3319e328
Andreas Ennemoser <andreas.ennemoser@aon.at>
2025-10-31 08:58:14 +0100

    Add coordinate labels to home page

--- src/gui/pages/page_home.cpp
+++ src/gui/pages/page_home.cpp
@@ -1,9 +1,37 @@
+#include "fonts.h"
 #include "page_home.h" // already brings lvgl.h in
+
+// extern const lv_font_t lv_font_roboto_mono_46;
+extern const lv_font_t lv_font_montserrat_24;
+
+lv_obj_t *lbl_x;
+lv_obj_t *lbl_y;
+lv_obj_t *lbl_z;
+lv_obj_t *lbl_cs;
 
 void page_home_create(lv_obj_t *parent)
 {
-    lv_obj_t *label = lv_label_create(parent);
-    lv_label_set_text(label, "HOME\nTap tabs below to switch pages");
-    lv_obj_set_style_text_color(label, lv_color_black(), LV_PART_MAIN);
-    lv_obj_align(label, LV_ALIGN_TOP_LEFT, 10, 10);
+    // --- X coordinate ---
+    lbl_x = lv_label_create(parent);
+    lv_label_set_text(lbl_x, "X +000.000");
+    lv_obj_set_style_text_font(lbl_x, &lv_font_montserrat_24, 0);
+    lv_obj_align(lbl_x, LV_ALIGN_TOP_MID, 0, 10);
+
+    // --- Y coordinate ---
+    lbl_y = lv_label_create(parent);
+    lv_label_set_text(lbl_y, "Y +000.000");
+    lv_obj_set_style_text_font(lbl_y, &lv_font_montserrat_24, 0);
+    lv_obj_align_to(lbl_y, lbl_x, LV_ALIGN_OUT_BOTTOM_MID, 0, 5);
+
+    // --- Z coordinate ---
+    lbl_z = lv_label_create(parent);
+    lv_label_set_text(lbl_z, "Z +000.000");
+    lv_obj_set_style_text_font(lbl_z, &lv_font_montserrat_24, 0);
+    lv_obj_align_to(lbl_z, lbl_y, LV_ALIGN_OUT_BOTTOM_MID, 0, 5);
+
+    // --- Coordinate-system label (smaller font) ---
+    lbl_cs = lv_label_create(parent);
+    lv_label_set_text(lbl_cs, "G53");
+    lv_obj_set_style_text_font(lbl_cs, &lv_font_montserrat_24, 0);
+    lv_obj_align(lbl_cs, LV_ALIGN_BOTTOM_RIGHT, -10, -10);
 }
